9a2cf88a2849e5fdb09f0d435e4e20ab3840835d
Erica Wright <ewright@mobify.com>
2016-02-22 15:15:55 -0800

    initial commit

---
+++ app/config/ui.js
@@ -0,0 +1,12 @@
+/**
+ * RequireJS paths for the ui bundle
+ */
+
+require.config({
+    'paths': {
+        'fastclick': 'bower_components/fastclick/lib/fastclick',
+        'deckard': 'bower_components/deckard/dist/deckard.min',
+        'pre-tag': 'bower_components/pre-tag/index',
+        'hijax': 'bower_components/hijax/dist/hijax'
+    }
+});
---
+++ app/global/router.js
@@ -0,0 +1,18 @@
+define([
+    '$',
+    'adaptivejs/router',
+    'pages/home/view',
+    'pages/category/view',
+    'pages/pdp/view'
+],
+function($, Router, Home, Category, PDP) {
+    var router = new Router();
+
+    router
+        .add(Router.selectorMatch('body.home'), Home)
+        .add(Router.selectorMatch('body.category'), Category)
+        .add(Router.selectorMatch('body.pdp'), PDP)
+        .add(function() {return true;}, Home);
+
+    return router;
+});
