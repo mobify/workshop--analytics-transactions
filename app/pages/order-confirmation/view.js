5bad609605e083cfd8c3f6907222d024f9ef391d
Erica Wright <ewright@mobify.com>
2016-02-23 15:30:53 -0800

    step 3 base

--- app/pages/order-confirmation/view.js
+++ app/pages/order-confirmation/view.js
@@ -10,10 +10,38 @@ function($, BaseView, template) {
 
         context: {
             templateName: 'order-confirmation',
-            body: function() {
-                return $('body');
-            }
-        }
+               title: function() {
+                   return $('h1').text();
+               },
+               items: function() {
+                   return $('#order-confirmation tr').filter(function() {
+                       var $row = $(this);
+                       return !$row.hasClass('subtotal') && !$row.hasClass('tax') && !$row.hasClass('total');
+                   }).map(function() {
+                       var $row = $(this);
+                       return {
+                           image: $row.find('td:nth-of-type(1) img').attr('x-src'),
+                           name: $row.find('td:nth-of-type(2)  p:first').text(),
+                           price: $row.find('td:nth-of-type(3)').text(),
+                           sku: $row.find('.sku').text()
+                       };
+                   });
+               },
+               transactionNumber: function() {
+                   return $('#transaction-number').text();
+               },
+               summary: function() {
+                   return $('#order-confirmation tr').filter(function() {
+                       return this.className;
+                   }).map(function() {
+                       var $row = $(this);
+                       return {
+                           name: $row.find('td:nth-of-type(2)').text(),
+                           amount: $row.find('td:last-of-type').text()
+                       };
+                   });
+               }
+           }
 
         /**
          * If you wish to override preProcess/postProcess in this view, have a look at the documentation:
